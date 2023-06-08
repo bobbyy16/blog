366bdf7fffafd97112eeaabd87dbd7a17c7c7c88
bobbyy16 <bobbyyyyy16@gmail.com>
2023-06-08 19:01:09 +0530

    useEffect hook

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,39 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import BlogList from './BlogList'
 
 export default function Home() {
-    const [name, setName] = useState('Abhishek');
-    const [age, setAge] = useState(23);
+    const [blogs, setBlogs] = useState(null)
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState(null)
 
-    const handleClick = () => {
-        setName('Bobs')
-        setAge(22)
-    }
+    
+    useEffect(() => {
+        setTimeout(() => {
+          fetch('http://localhost:8000/blogs')
+          .then(res => {
+            if (!res.ok) { // error coming back from server
+              throw Error('could not fetch the data for that resource');
+            } 
+            return res.json();
+          })
+          .then(data => {
+            setIsPending(false);
+            setBlogs(data);
+            setError(null);
+          })
+          .catch(err => {
+            // auto catches network / connection error
+            setIsPending(false);
+            setError(err.message);
+          })
+        }, 1000);
+      }, [])
+
   return (
     <div className='home'>
-      <h2>Homepage</h2>
-      <p>{name} is {age} years old</p>
-      <button onClick={handleClick}>click me</button>
+        {error && <div>{error}</div> }
+        {isPending && <div>Loading....</div>}
+        {blogs && <BlogList blogs={blogs} title='All blogs'/>}    
     </div>
   )
 }
